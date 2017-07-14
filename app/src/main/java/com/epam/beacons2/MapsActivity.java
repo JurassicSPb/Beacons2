54775387a2ed2b250341a8b6d650963109cca81c
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-07-14 15:56:46 +0300

    integr with ibeacon

--- app/src/main/java/com/epam/beacons2/MapsActivity.java
+++ app/src/main/java/com/epam/beacons2/MapsActivity.java
@@ -1,12 +1,22 @@
 package com.epam.beacons2;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.location.Location;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.epam.beacons2.util.BleUtil;
+import com.epam.beacons2.util.ScannedDevice;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,8 +33,12 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, BluetoothAdapter.LeScanCallback{
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
@@ -34,27 +48,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private double latitude = 0.0;
     private double longitude = 0.0;
 
+    private BluetoothAdapter mBTAdapter;
+    private boolean mIsScanning;
+    private ArrayList <ScannedDevice> scannedDevices = new ArrayList<>();
+
     @Override
-    @SuppressWarnings("MissingPermission")
+//    @SuppressWarnings("MissingPermission")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            Log.d(MapsActivity.class.getSimpleName(), "herehere"  + latitude);
-                            longitude = location.getLongitude();
-                            Log.d(MapsActivity.class.getSimpleName(), "herehere " + longitude);
-                        }
-                    }
-                });
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//
+//        mFusedLocationClient.getLastLocation()
+//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location location) {
+//                        // Got last known location. In some rare situations this can be null.
+//                        if (location != null) {
+//                            latitude = location.getLatitude();
+//                            Log.d(MapsActivity.class.getSimpleName(), "herehere"  + latitude);
+//                            longitude = location.getLongitude();
+//                            Log.d(MapsActivity.class.getSimpleName(), "herehere " + longitude);
+//                        }
+//                    }
+//                });
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -71,8 +89,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .image(BitmapDescriptorFactory.fromResource(R.drawable.map2))
                 .position(epamLoc, 110f, 80f);
 
+        init();
+        startScan();
 
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -116,7 +137,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         new LatLng(59.9851017, 30.3097383),
                         new LatLng(59.9851217, 30.3097483),
                         new LatLng(59.9851217, 30.3096483),
-                        new LatLng(59.9851017, 30.3096383)
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(59.9851017, 30.3096383),
+                        new LatLng(60, 30.3096383)
                 ));
 
         mMap.setOnGroundOverlayClickListener(new GoogleMap.OnGroundOverlayClickListener() {
@@ -129,10 +166,125 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
 
+
+
 //                GroundOverlay imageOverlay2 = mMap.addGroundOverlay(groundOverlayOptions2);
 
             }
         });
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if ((mBTAdapter != null) && (!mBTAdapter.isEnabled())) {
+            Toast.makeText(this, R.string.bt_not_enabled, Toast.LENGTH_SHORT).show();
+//            invalidateOptionsMenu();
+        } else startScan();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        stopScan();
+    }
+
+    private void init() {
+        // BLE check
+        if (!BleUtil.isBLESupported(this)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        BluetoothManager manager = BleUtil.getManager(this);
+        if (manager != null) {
+            mBTAdapter = manager.getAdapter();
+
+        }
+        if (mBTAdapter == null) {
+            Toast.makeText(this, R.string.bt_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        stopScan();
+    }
+
+    private void startScan() {
+        if ((mBTAdapter != null) && (!mIsScanning)) {
+            mBTAdapter.startLeScan(this);
+            mIsScanning = true;
+//            setProgressBarIndeterminateVisibility(true);
+//            invalidateOptionsMenu();
+        }
+    }
+
+    private void stopScan() {
+        if (mBTAdapter != null) {
+            mBTAdapter.stopLeScan(this);
+        }
+        mIsScanning = false;
+//        setProgressBarIndeterminateVisibility(false);
+//        invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onLeScan(final BluetoothDevice newDeivce, final int newRssi,
+                         final byte[] newScanRecord) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+//                Log.d(MapsActivity.class.getSimpleName(), "herehere");
+//                String summary = mDeviceAdapter.update(newDeivce, newRssi, newScanRecord);
+                String summary = newDeivce.toString();
+                Log.d(MapsActivity.class.getSimpleName(), "herehere " + summary);
+//                scannedDevices.add(new ScannedDevice(newDeivce, newRssi, newScanRecord));
+                update(newDeivce, newRssi, newScanRecord);
+
+            }
+        });
+    }
+
+    public void update(BluetoothDevice newDevice, int rssi, byte[] scanRecord) {
+        if ((newDevice == null) || (newDevice.getAddress() == null)) {
+            return;
+        }
+
+        boolean contains = false;
+        for (ScannedDevice device : scannedDevices) {
+            if (newDevice.getAddress().equals(device.getDevice().getAddress())) {
+                contains = true;
+                // update
+                device.setRssi(rssi);
+                device.setScanRecord(scanRecord);
+                break;
+            }
+        }
+        if (!contains) {
+            // add new BluetoothDevice
+            scannedDevices.add(new ScannedDevice(newDevice, rssi, scanRecord));
+//            Log.d(MapsActivity.class.getSimpleName(), "herehere" + scannedDevices.get(0).getDisplayName());
+        }
+
+        // sort by RSSI
+        Collections.sort(scannedDevices, new Comparator<ScannedDevice>() {
+            @Override
+            public int compare(ScannedDevice lhs, ScannedDevice rhs) {
+                if (lhs.getRssi() == 0) {
+                    return 1;
+                } else if (rhs.getRssi() == 0) {
+                    return -1;
+                }
+                if (lhs.getRssi() > rhs.getRssi()) {
+                    return -1;
+                } else if (lhs.getRssi() < rhs.getRssi()) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
     }
 }
